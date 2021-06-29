ebb6cebd103de16b6b532e3f5edd3e93d1726502
Zion98 <adeyemioluwasegun98@gmail.com>
2021-06-29 12:26:31 +0100

    form design

--- src/auth/Signin.tsx
+++ src/auth/Signin.tsx
@@ -4,71 +4,79 @@ import Form from '../styles/Form';
 import { signIn } from './api-auth';
 import { setCookie } from '../utils/setCookie';
 import Swal from 'sweetalert2';
-
+import img1 from '../assets/img2.jpg';
+import img3 from '../assets/imag3.jpg';
 export default function Signin() {
-  const [values, setValues] = useState({
-    email: '',
-    password: '',
-    error: '',
-  });
+	const [values, setValues] = useState({
+		email: '',
+		password: '',
+		error: '',
+	});
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setValues({ ...values, [name]: value });
-  };
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target;
+		setValues({ ...values, [name]: value });
+	};
 
-  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    const user = {
-      email: values.email,
-      password: values.password,
-    };
+	const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+		const user = {
+			email: values.email,
+			password: values.password,
+		};
 
-    let response = await signIn(user);
+		let response = await signIn(user);
 
-    if (response?.user) {
-      setCookie(response.token);
-      window.location.href = '/stores/all';
-    } else {
-      Swal.fire('Error', response?.error);
-    }
-  };
+		if (response?.user) {
+			setCookie(response.token);
+			window.location.href = '/stores/all';
+		} else {
+			Swal.fire('Error', response?.error);
+		}
+	};
 
-  return (
-    <div className="auth-form">
-      <Form>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            name="email"
-            id="email"
-            value={values.email}
-            placeholder="Email"
-            onChange={handleChange}
-            type="text"
-            required
-          />
-        </div>
+	return (
+		<div>
+			<div className="auth-form">
+				<Form>
+					<p className="form-title">Account SignIn</p>
+					<div>
+						{/* <label htmlFor="email">Email</label> */}
+						<input
+							name="email"
+							id="email"
+							value={values.email}
+							placeholder="Email"
+							onChange={handleChange}
+							type="text"
+							required
+						/>
+					</div>
 
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            name="password"
-            id="password"
-            value={values.password}
-            placeholder="Password"
-            onChange={handleChange}
-            type="password"
-            required
-          />
-        </div>
+					<div>
+						{/* <label htmlFor="password">Password</label> */}
+						<input
+							name="password"
+							id="password"
+							value={values.password}
+							placeholder="Password"
+							onChange={handleChange}
+							type="password"
+							required
+						/>
+					</div>
 
-        <p>
-          Don't have an account? <Link to="/register">Register</Link>
-        </p>
+					<p>
+						Don't have an account? <Link to="/register">Register</Link>
+					</p>
 
-        <button onClick={handleSubmit}>SignIn</button>
-      </Form>
-    </div>
-  );
+					<button onClick={handleSubmit}>SignIn</button>
+				</Form>
+				<div className="market">
+					<img src={img3} alt="market" />
+					<img className="top" src={img1} alt="market" />
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/styles/Form.ts
+++ src/styles/Form.ts
@@ -13,79 +13,93 @@ const loading = keyframes`
 `;
 
 const Form = styled.form`
-  box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.05);
-  padding: 20px;
-  font-size: 1.5rem;
-  line-height: 1.5;
-  font-weight: 600;
-  margin: 0 5em;
-  width: 100%;
-  a {
-    text-decoration: none;
-    color: red;
-  }
-  label {
-    display: block;
-    color: red;
-  }
-  p {
-    font-size: 1rem;
-    margin: 1em 2em;
-  }
-  div {
-    margin: 0 2em;
-  }
-  button {
-    margin-left: 2.5em;
-    margin-right: 10em;
-  }
-  input,
-  textarea,
-  select {
-    width: 100%;
-    padding: 0.5rem;
-    margin-bottom: 2rem;
-    font-size: 1rem;
-    border: 1px solid black;
-    &:focus {
-      outline: 0;
-      border-color: var(--red);
-    }
-  }
-  button,
-  input[type='submit'] {
-    max-width: 92.5%;
-    background: red;
-    color: white;
-    border: 0;
-    font-size: 1.25rem;
-    font-weight: 600;
-    padding: 0.5rem 1.2rem;
-    border-radius: 7px 7px 7px 7px;
-  }
-  fieldset {
-    border: 0;
-    padding: 0;
+	box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.05);
+	padding: 5rem;
+	font-size: 1.5rem;
+	line-height: 1.5;
+	font-weight: 600;
+	margin: 0 auto;
+	max-width: 60%;
+	background-color: #fff;
 
-    &[disabled] {
-      opacity: 0.5;
-    }
-    &::before {
-      height: 10px;
-      content: '';
-      display: block;
-      background-image: linear-gradient(
-        to right,
-        #ff3019 0%,
-        #e2b04a 50%,
-        #ff3019 100%
-      );
-    }
-    &[aria-busy='true']::before {
-      background-size: 50% auto;
-      animation: ${loading} 0.5s linear infinite;
-    }
-  }
+	.form-title {
+		font-size: 1.5rem;
+		color: rgb(237, 72, 81);
+		text-align: center;
+		font-weight: 900;
+	}
+	a {
+		text-decoration: none;
+		color: red;
+	}
+	label {
+		display: block;
+		color: red;
+	}
+	p {
+		font-size: 1rem;
+		margin: 1em 2em;
+	}
+	div {
+		margin: 0 2em;
+	}
+	button {
+		margin-left: 2.5em;
+		margin-right: 10em;
+	}
+	input,
+	textarea,
+	select {
+		width: 100%;
+		padding: 0.5rem;
+		margin-bottom: 1rem;
+		font-size: 1rem;
+		border: 2px solid rgb(228, 233, 245);
+		border-radius: 6px;
+		&:focus {
+			outline: none;
+			border: none;
+			border-color: var(--red);
+		}
+	}
+	input::placeholder {
+		color: rgba(0, 0, 0, 0.5);
+		font-size: 1rem;
+	}
+	button,
+	input[type='submit'] {
+		max-width: 92.5%;
+		background: red;
+		color: white;
+		border: none;
+		font-size: 1.25rem;
+		font-weight: 600;
+		padding: 0.5rem 1.2rem;
+		border-radius: 7px 7px 7px 7px;
+	}
+
+	fieldset {
+		border: 0;
+		padding: 0;
+
+		&[disabled] {
+			opacity: 0.5;
+		}
+		&::before {
+			height: 10px;
+			content: '';
+			display: block;
+			background-image: linear-gradient(to right, #ff3019 0%, #e2b04a 50%, #ff3019 100%);
+		}
+		&[aria-busy='true']::before {
+			background-size: 50% auto;
+			animation: ${loading} 0.5s linear infinite;
+		}
+	}
+
+	@media screen and (max-width: 767px) {
+		max-width: 50%;
+	}
 `;
 
 export default Form;
